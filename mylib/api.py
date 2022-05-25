5bf761333587a4a44cc42f67f8a77e8d58836d07
Saul Tapia <rodrigo.1010stc@gmail.com>
2022-05-24 22:39:47 -0400

    cambios en las api

--- mylib/api.py
+++ mylib/api.py
@@ -1,15 +1,13 @@
 import requests
 import json
 
-def sendPost(entrada, salida, total):
+def sendPost(entrada, salida):
 
     url = "http://192.168.100.218:4000/vision"
-    # data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     data = {
-        "Entrada": entrada,
-        "Salida": salida,
-        "Total": total
+        "entrada": entrada,
+        "salida": salida
     }
     r = requests.post(url, data=json.dumps(data), headers=headers)
     return r.status_code
@@ -17,3 +15,13 @@ def sendPost(entrada, salida, total):
 # PRueba de cliente post con json
 # respuesta = sendPost("entrada", "salida", "total")
 # print(respuesta)
+
+def sendPostTotal(total):
+
+    url = "http://192.168.100.218:4000/total"
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    data = {
+        "total": total
+    }
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    return r.status_code
